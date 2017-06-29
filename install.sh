325282624c489830371877af8d9a609e59801765
simonVansuyt <simon.vansuyt@gmail.com>
2017-06-29 21:47:56 +0200

    init

--- install.sh
+++ install.sh
@@ -1,11 +1,27 @@
 #!/bin/bash
 
 ## Make sure that you have a internet connection on your pi
+## run this script as root
 
-# Git
-sudo apt-get install git
+# git
+apt-get install git-all
+# ask for git name and email
+echo -e "\x1B[1;33m[GIT USER]\x1B[0m"
+read -rp "What is your user.name? " name
+read -rp "What is your user.email? " email
+git config --global user.name ${name}
+git config --global user.email ${email}
 
-# Vim
-sudo apt-get install vim
+# vim
+apt-get install vim
+mv -rf ./.vim ~/.vim
+mv -rf ./.vimrc ~/.vimrc
 
-# Node
+# nodejs
+apt-get install nodejs nodejs-legacy
+apt-get install npm
+# npm permission fix
+chown -R $(whoami) $(npm config get prefix)/{lib/node_modules,bin,share}
+
+# rasperry pi camera python plugin
+apt-get install python-picamera
